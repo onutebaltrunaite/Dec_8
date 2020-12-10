2323b9c527f23db6dc17bdcff3e119aaf9324003
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-10 12:53:06 +0200

    dar pora uzduociu

--- index.php
+++ index.php
@@ -8,10 +8,9 @@ date_default_timezone_set('Europe/Vilnius');
 $stringas = 'sudas kazkokie zodziai rupuze kazkokie rupuze zodziai ir vel rupuze';
 $keiksmazodis = ['rupuze', 'sudas'];
 
-
-$kintamieji = ['kompiuteris', -235, '1.235eght', 123.45, 567.6, 'dar kazkas 123', 'kazkas'];
-
-$floatReiksmes = [];
+$numbers = [];
+$suma = 0;
+$smailuTekstas = 'cia yra smailu :-D tektas, reikia pakeisti :-) smailus i paveikslelius :-( bla bla bla';
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +18,7 @@ $floatReiksmes = [];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/42762d49f1.js" crossorigin="anonymous"></script>
     <title> Title </title>
 <style>
 
@@ -33,50 +33,101 @@ $floatReiksmes = [];
 
     <?php print (str_replace($keiksmazodis,"***",$stringas)); ?>
 
-
-    <!-- <div><?php print($item) ?></div> -->
-
-    <?php foreach($kintamieji as $item){
-        if (is_float($item)){
-            $floatReiksmes[] = floatval($item);
-        }
-    } ?>
-
-    <?php var_dump($floatReiksmes); ?>
-    <?php print_r($floatReiksmes); ?>
-
+    
 
 
 
     <?php  
     $zodziai = 'reikia parasyti bent desimt zodziu reikia parasyti bent desimt zodziu';
     $suskaidytas = explode(" ", $zodziai);
+    // $didelis = strtoupper($suskaidytas);
     print_r($suskaidytas);
     ?>
+    <?php foreach($suskaidytas as $zodis): ?>
+    <p><?php print strtoupper($zodis); ?></p>
+    <?php endforeach; ?>
+
+
+
+
+
+    <?php
+        for ($x = 0; $x < 10; $x++){
+            $numbers[] = rand(0, 100);
+        }
+        var_dump($numbers);
+
+        foreach ($numbers as $number) {
+            if($number % 2 == 0){
+                $suma += $number;
+            }
+        }; 
+        print $suma;
+    ?>
+
+
+    
+
+        <!-- NEEEE -->
+    <!-- <?php foreach($numbers as $number):?>
+    <?php if($number % 2 == 0): ?>
+    <p><?php print $number; ?></p>
+    <?php endif; ?>
+    <?php endforeach; ?> -->
+
+
+
+        <!-- NEVEIKS -->
+        <!-- <?php foreach ($smailuTekstas as $zod) {
+            if($zod == ':-)') {
+                $smailuTekstas = str_replace(':-)', '<i class="far fa-smile"></i>', $smailuTekstas);
+            }
+            if($zod == ':-D') {
+                $smailuTekstas = str_replace(':-D', '<i class="fas fa-grin-wink"></i>', $smailuTekstas);
+            }
+            if($zod == ':-(') {
+                $smailuTekstas = str_replace(':-(', '<i class="far fa-frown-open"></i>', $smailuTekstas);
+            }
+        };
+        ?> -->
+
+
+
+
+        <?php 
+            $smailuTekstas = str_replace(':-)', '<i class="far fa-smile"></i>', $smailuTekstas);
+            $smailuTekstas = str_replace(':-D', '<i class="fas fa-grin-wink"></i>', $smailuTekstas);
+            $smailuTekstas = str_replace(':-(', '<i class="far fa-frown-open"></i>', $smailuTekstas);
+            print $smailuTekstas;
+        ?>
+        
+    
+
+        
+     
+    
 
 </body>
 </html>
 
-<!-- //1. Sukurkite masyva su betkokiais kintamaisiais. Kintamuju turi buti ne maziau nei 10.
-//2. Sukurkite nauja masyva kuris tures tik float reiksmes perimtas is pirmojo masyvo.
-//3. Atvaizduokite reiksmes skirtingose eilutese.
-//Panaudoti is_float -->
 
 
-<!-- //1. Sukurkite kintamaji is 10 zodziu.
-//2. Sio kintamojo zodzius sudeliokite i masyva.
-//3. Masyve esancius zodzius atvaizduoti ekrane visomis didziosiomis raidemis.
-//Panaudoti explode, strtoupper
+
+
+<!-- 
 //1. Sukurti masyva is 10 atsitiktiniu skaiciu nuo 0 iki 100.
 //2. Patikrinti masyve esancias reiksmes ar jos yra lygines ir jei jos lygines visas jas sudejus atvaizduoti
 //ekrane.
 //Panaudojant %
+
+
 // 1. Sukurkite funkcija, kuri priimtu viena argumenta - $tekst ir tekste pakeistu smail`us (:-), :-D, :-(, ir tt.) i paveiksliukus.
 //Galima pasinaudoti: https://www.pic4ever.com/ (str_replace pades)//Panaudoti str_replace
+
 // 1. Sukurkite salyga kuri priimtu viena parametra $text ir grazintu teksta.
 //Jeigu tekstas yra ilgesnis negu 100 simboliu, tekstas turetu buti apkirptas is desines ir prideti trys taskai (...).
 //Grazinto teksto ilgis turi nevirsyti 100 simboliu kartu su taskais.
 //Panaudoti mb_strlen ir mb_substr
 //2. Papildykite 1 uzduoti taip, kad tekstas nebutu nukirptas ties zodzio vidurio o ties tarpu.
 ////Grazinto teksto ilgis gali buti mazesnis uz 100 simboliu, bet svarbiausia ne didesnis.
-/// Panaudoti mb_strlen, mb_substr, mb_strrpos, -->
+/// Panaudoti mb_strlen, mb_substr, mb_strrpos, --> 
