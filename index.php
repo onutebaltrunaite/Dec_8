a821ff1b7719fa259810ce8cbfa42d30144b4c12
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-11 09:01:09 +0200

    first commit

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
-<?php 
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-date_default_timezone_set('Europe/Vilnius');
 
+include_once 'php.php';
 
-$stringas = 'sudas kazkokie zodziai rupuze kazkokie rupuze zodziai ir vel rupuze';
-$keiksmazodis = ['rupuze', 'sudas'];
-
-$numbers = [];
-$suma = 0;
-$smailuTekstas = 'cia yra smailu :-D tektas, reikia pakeisti :-) smailus i paveikslelius :-( bla bla bla';
+// var_dump($arrayGallery);
 ?>
 
 <!DOCTYPE html>
@@ -18,116 +13,45 @@ $smailuTekstas = 'cia yra smailu :-D tektas, reikia pakeisti :-) smailus i pavei
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/42762d49f1.js" crossorigin="anonymous"></script>
-    <title> Title </title>
-<style>
-
-</style>
-
-</head>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
+    <header id='home'>
+        <div class='nav'>
+            <?php foreach ($array['navBar'] as $item): ?>
+            <a href="<?php print $item['id']?>" class='btnStyle'><?php print $item['name']; ?></a>
+            <?php endforeach; ?>
+        </div>
+    </header>
+    <main>
+        <section id='about'>
+            <h1>ABOUT</h1>
+            <?php foreach ($array['arrayAbout'] as $item): ?>
+            <img src="<?php print $item['logo']; ?>" alt="">
+            <div><?php print $item['about']; ?></div>  
+            <?php endforeach; ?>
+        </section>
+        <section id='gallery'>
+            <h1>GALLERY</h1>
+            <div class='d-flex'>
+                <?php foreach ($array['arrayGallery'] as $item): ?>
+                <img src="<?php print $item['picture']; ?>" alt="">
+                <div><?php print $item['about']; ?></div>
+                <?php endforeach; ?> 
+            </div>
+        </section>
+    </main>
+    <footer id='contacts'>
+        <h1>CONTACTS</h1>
+        <?php foreach ($array['arrayContacts'] as $item): ?>
+            <p><?php print $item['phone'] ?></p>
+            <p><?php print $item['address'] ?></p>
+        <?php endforeach; ?>   
+    </footer>
 
 
-
-    <?php print (str_replace($keiksmazodis,"***",$stringas)); ?>
-
-    
-
-
-
-    <?php  
-    $zodziai = 'reikia parasyti bent desimt zodziu reikia parasyti bent desimt zodziu';
-    $suskaidytas = explode(" ", $zodziai);
-    // $didelis = strtoupper($suskaidytas);
-    print_r($suskaidytas);
-    ?>
-    <?php foreach($suskaidytas as $zodis): ?>
-    <p><?php print strtoupper($zodis); ?></p>
-    <?php endforeach; ?>
-
-
-
-
-
-    <?php
-        for ($x = 0; $x < 10; $x++){
-            $numbers[] = rand(0, 100);
-        }
-        var_dump($numbers);
-
-        foreach ($numbers as $number) {
-            if($number % 2 == 0){
-                $suma += $number;
-            }
-        }; 
-        print $suma;
-    ?>
-
-
-    
-
-        <!-- NEEEE -->
-    <!-- <?php foreach($numbers as $number):?>
-    <?php if($number % 2 == 0): ?>
-    <p><?php print $number; ?></p>
-    <?php endif; ?>
-    <?php endforeach; ?> -->
-
-
-
-        <!-- NEVEIKS -->
-        <!-- <?php foreach ($smailuTekstas as $zod) {
-            if($zod == ':-)') {
-                $smailuTekstas = str_replace(':-)', '<i class="far fa-smile"></i>', $smailuTekstas);
-            }
-            if($zod == ':-D') {
-                $smailuTekstas = str_replace(':-D', '<i class="fas fa-grin-wink"></i>', $smailuTekstas);
-            }
-            if($zod == ':-(') {
-                $smailuTekstas = str_replace(':-(', '<i class="far fa-frown-open"></i>', $smailuTekstas);
-            }
-        };
-        ?> -->
-
-
-
-
-        <?php 
-            $smailuTekstas = str_replace(':-)', '<i class="far fa-smile"></i>', $smailuTekstas);
-            $smailuTekstas = str_replace(':-D', '<i class="fas fa-grin-wink"></i>', $smailuTekstas);
-            $smailuTekstas = str_replace(':-(', '<i class="far fa-frown-open"></i>', $smailuTekstas);
-            print $smailuTekstas;
-        ?>
-        
-    
-
-        
-     
-    
 
 </body>
 </html>
-
-
-
-
-
-<!-- 
-//1. Sukurti masyva is 10 atsitiktiniu skaiciu nuo 0 iki 100.
-//2. Patikrinti masyve esancias reiksmes ar jos yra lygines ir jei jos lygines visas jas sudejus atvaizduoti
-//ekrane.
-//Panaudojant %
-
-
-// 1. Sukurkite funkcija, kuri priimtu viena argumenta - $tekst ir tekste pakeistu smail`us (:-), :-D, :-(, ir tt.) i paveiksliukus.
-//Galima pasinaudoti: https://www.pic4ever.com/ (str_replace pades)//Panaudoti str_replace
-
-// 1. Sukurkite salyga kuri priimtu viena parametra $text ir grazintu teksta.
-//Jeigu tekstas yra ilgesnis negu 100 simboliu, tekstas turetu buti apkirptas is desines ir prideti trys taskai (...).
-//Grazinto teksto ilgis turi nevirsyti 100 simboliu kartu su taskais.
-//Panaudoti mb_strlen ir mb_substr
-//2. Papildykite 1 uzduoti taip, kad tekstas nebutu nukirptas ties zodzio vidurio o ties tarpu.
-////Grazinto teksto ilgis gali buti mazesnis uz 100 simboliu, bet svarbiausia ne didesnis.
-/// Panaudoti mb_strlen, mb_substr, mb_strrpos, --> 
---
+++ php.php
@@ -0,0 +1,60 @@
+<?php
+
+$array = [
+    'navBar' => [
+        [
+            'id' => '#home',
+            'name' => 'Home'
+        ],
+        [
+            'id' => '#about',
+            'name' => 'About'
+        ],
+        [
+            'id' => '#gallery',
+            'name' => 'Gallery'
+        ],
+        [
+            'id' => '#contacts',
+            'name' => 'Contacts'
+        ]
+    ],
+    'arrayAbout' => [
+        [
+            'logo' => 'images/bomb.jpg',
+            'about' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, repudiandae. Sunt, fugit nihil modi blanditiis repellendus alias fuga accusamus minus rerum doloremque inventore, architecto fugiat minima soluta corrupti. Reprehenderit, minus.'
+        ]
+    ],
+    'arrayGallery' => [
+        [
+            'picture' => 'https://img.freepik.com/free-vector/bomb-tnt-vector-clip-art-illustration_138676-88.jpg?size=626&ext=jpg&ga=GA1.2.1534020743.1601078400',
+            'about' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, repudiandae. Sunt, fugit nihil modi blanditiis repellendus alias fuga accusamus minus rerum doloremque inventore, architecto fugiat minima soluta corrupti. Reprehenderit, minus.'
+        ],
+        [
+            'picture' => 'https://cdn3.vectorstock.com/i/1000x1000/08/02/word-pow-with-bomb-explosive-vector-16950802.jpg',
+            'about' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, repudiandae. Sunt, fugit nihil modi blanditiis repellendus alias fuga accusamus minus rerum doloremque inventore, architecto fugiat minima soluta corrupti. Reprehenderit, minus.'
+        ],
+        [
+            'picture' => 'https://cdn5.vectorstock.com/i/1000x1000/50/39/funny-cartoon-bomb-with-cigar-vector-1895039.jpg',
+            'about' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, repudiandae. Sunt, fugit nihil modi blanditiis repellendus alias fuga accusamus minus rerum doloremque inventore, architecto fugiat minima soluta corrupti. Reprehenderit, minus.'
+        ],
+        [
+            'picture' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKtKx6Cu--HmKz626sz1E8fO6cfTgVxZ5QWg&usqp=CAU',
+            'about' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, repudiandae. Sunt, fugit nihil modi blanditiis repellendus alias fuga accusamus minus rerum doloremque inventore, architecto fugiat minima soluta corrupti. Reprehenderit, minus.'
+        ]
+    ],
+    'arrayContacts' => [
+        [
+            'address' => 'Pakalnuciu 3, Rokiskis',
+            'phone' => '666-666-666'
+        ],
+        [
+            'address' => 'Topoliu 3, Rokiskis',
+            'phone' => '666-666-666'
+        ]
+    ]
+
+];
+
+
+?>
