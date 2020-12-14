7ae02ab8d184667a9ba1d31790ff766c766b9e22
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-14 12:23:31 +0200

    prisidejo while

--- index.php
+++ index.php
@@ -3,28 +3,31 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$savaitesDiena = 'apsiniauke';
-
-switch ($savaitesDiena){
-    case 'sauleta';
-    $src = 'https://solarsystem.nasa.gov/system/basic_html_elements/11561_Sun.png';
-    break;
-    case 'lietus';
-    $src = 'https://geografija.lt/wp-content/uploads/2020/06/lietus.jpg';
-    break;
-    case 'apsiniauke';
-    $src = 'https://www.efoto.lt/files/images/52803/varniu%20pap%20zemesne.jpg';
-    break;
-    case 'audra';
-    print 'KETVIRTADIENIS';
-    break;
-    case 'giedra';
-    print 'PENKTADIENIS';
-    break;
-    default:
-    print 'Toks oras neegzistuoja';
-}
-
+// $savaitesDiena = 'giedra';
+// switch ($savaitesDiena){
+//     case 'sauleta';
+//     $src = 'https://solarsystem.nasa.gov/system/basic_html_elements/11561_Sun.png';
+//     break;
+//     case 'lietus';
+//     $src = 'https://geografija.lt/wp-content/uploads/2020/06/lietus.jpg';
+//     break;
+//     case 'apsiniauke';
+//     $src = 'https://www.efoto.lt/files/images/52803/varniu%20pap%20zemesne.jpg';
+//     break;
+//     case 'audra';
+//     $src = 'images/bomb.jpg';
+//     break;
+//     case 'giedra';
+//     $src = 'https://bloximages.chicago2.vip.townnews.com/tucson.com/content/tncms/assets/v3/editorial/3/fb/3fb92eca-31bd-11e6-92d5-c3dd26bc749c/575f3fa80552e.image.jpg?resize=1200%2C878';
+//     break;
+//     default:
+//     print 'Toks oras neegzistuoja';
+// }
+// $x = 0;
+// while ($x < 5){
+//     print 'Spausdink <br>';
+//     $x++;
+// }
 
 ?>
 
@@ -39,7 +42,7 @@ switch ($savaitesDiena){
 </head>
 <body>
 
-<img src="<?php print $src;?>" alt="">
+<!-- <img src="<?php print $src;?>" alt=""> -->
 
 
 
