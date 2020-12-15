35f86c2bcc9116a4651efe02b0f8b2cfb5bd2e0f
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-15 16:21:57 +0200

    15DEC NAUJOS uzduotys #5 #6 #7 #8 cigaretes ir FOR CYCLE

--- index.php
+++ index.php
@@ -6,25 +6,44 @@ date_default_timezone_set('Europe/Vilnius');
 
 
 
-$months = 50;
-$car_price_new = 30000;
-$depreciation = 2;
-
-$car_price_used = $car_price_new;
-
-$santaupos = 15000;
-$number = 0;
-for($x = 0; $x < $months; $x++){
-    if ($santaupos <= $car_price_used){
-        $car_price_used -= $car_price_used * $depreciation / 100;  
-        $number = $x;
-    } else {
-    break;
-    }
-};
+$days = 365;
+$cigs_mon_fri = rand(3, 4);
+$cigs_sat = rand(10, 20);
+$cigs_sun = rand(1, 3);
+$pack_price = 3.50;
+$time_per_cig = 5; //5min
 
 
-// $depr_perc = 100 - ($car_price_used * 100 / $car_price_new);
+$sum = 0;
+$price = 0;
+
+$sumasuma = 0;
+$price2 = 0;
+
+for ($week = 1; $week <= 52; $week++){
+    $monday = rand(3, 4);
+    $thuesday = rand(3, 4);
+    $wednesday = rand(3, 4);
+    $thursday = rand(3, 4);
+    $friday = rand(3, 4);
+    $saturday = rand(10, 20);
+    $sunday = rand(1, 3);  
+
+    $week_sum = $monday + $thuesday + $wednesday + $thursday + $friday + $saturday + $sunday;
+
+    $sum += $week_sum;
+
+    $weeksumasuma = $monday + $thuesday + $wednesday + $thursday + $friday;
+
+    $sumasuma += $weeksumasuma;
+}
+
+$price = ceil($sum/10) * $pack_price;
+$price2 = ceil($sumasuma/10) * $pack_price;
+
+$cizai = ceil($sum/10);
+
+$total = round($sum * $time_per_cig / 60);
 
 
 
@@ -39,16 +58,25 @@ for($x = 0; $x < $months; $x++){
     <link rel="stylesheet" href="style.css">
 </head>
 <style>
-
+.cizas {
+    background-image: url('images/cizas.jpg')
+}
 </style>
 
 <body>
 
-<h1>Kiek nuvertes masina?</h1>
-<h2>Naujos masinos kaina: <?php print $car_price_new; ?></h2>
+<h1>Mano dumu sskaiciuokle</h1>
+<h2>Per <?php print $days; ?> dienas, surukysiu <?php print $sum; ?> cigareciu uz <?php print $price; ?> eur.</h2>
 
+<h3>Nerukydamas darbo dienomis sutaupyciau <?php print $price2; ?> eur.</h3>
 
-<h3>Masina galesiu nusipirkti po <?php print $number; ?> menesiu, kai jos verte bus: <?php print round($car_price_used); ?> eur.</h3>
+<h3>Viso traukdamas kiekviena diena prastovesiu valandu <?php print $total;?></h3>
+
+<div style="display: flex; flex-wrap: wrap;">
+    <?php for ($x = 1; $x <= $cizai; $x++): ?>
+        <div style="height: 50px; width: 50px; background-size: cover; margin: 5px; border: 1px solid black; padding: 5px;" class="cizas"></div>
+    <?php endfor; ?>
+</div>
 
 </body>
 </html>
