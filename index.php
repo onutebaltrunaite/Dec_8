3a29037a8457fbe2fa6b96d231d543094f5066f3
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-15 10:54:11 +0200

    15DEC NAUJOS uzduotis #2

--- index.php
+++ index.php
@@ -4,11 +4,12 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set('Europe/Vilnius');
 
+$distance = rand(100, 500); //km
+$consumption = 7.5;         // 1/100km
+$price_1 = 1.3;             // Eur/1
 
-$string = 'Labas labas labas';
-$int = 12;
-$bool = true;
-$null = null;
+$fuel_total = $distance / 100 * $consumption;
+$price_trip = $fuel_total * $price_1;
 
 ?>
 
@@ -19,20 +20,19 @@ $null = null;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PAVADINIMAS</title>
     <link rel="stylesheet" href="style.css">
-
 </head>
 <style>
-
 </style>
 
 <body>
 
+<h1>Kelionės skaičiuoklė</h1>
 <ul>
-    <li><?php print $string; ?></li>
-    <li><?php print $int; ?></li>
-    <li><?php print $bool; ?></li>
-    <li><?php print $null; ?></li>
+    <li>Nuvažiuota distancija: <?php print $distance; ?></li>
+    <li>Sunaudota <?php print $fuel_total; ?> l. kuro.</li>
+    <li>Kaina: <?php print $price_trip; ?> pinigų.</li>
 </ul>
+
 
 </body>
 </html>
