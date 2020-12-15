b372d05d002bef70ac42249b176b9d5d6bb12bfd
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-15 10:36:08 +0200

    15DEC uzduotis x (su bomba)

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set('Europe/Vilnius');
 
+$page = $_SERVER['PHP_SELF'];
+$sec = "1";
+header("Refresh: $sec; url=$page");
 ?>
 
 <!DOCTYPE html>
@@ -16,32 +19,13 @@ date_default_timezone_set('Europe/Vilnius');
 
 </head>
 <style>
-.dice1 {
-    background-image: url(images/dice1.png);
-    
-}
-.dice2 {
-    background-image: url(images/dice2.png);
-}
-.dice3 {
-    background-image: url(images/dice3.png);
-}
-.dice4 {
-    background-image: url(images/dice4.png);
-}
-.dice5 {
-    background-image: url(images/dice5.png);
-}
-.dice6 {
-    background-image: url(images/dice6.png);
-}
+
 </style>
 
 <body>
 
-<div style="height: 200px; width: 200px; background-size: cover;" class="dice<?php print rand(1, 6); ?>"></div>
-
-
+<img style="width: <?php print date('s') . '0'; ?>px;" src="images/bomb.jpg" alt="">
+<div><?php print date('s');?></div>
 
 
 
