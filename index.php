2ca44555b118943c5f252df3c03592114d15dfc1
Onutė Baltrūnaitė <baltrunaiteo@gmail.com>
2020-12-15 17:00:38 +0200

    15DEC NAUJOS #1 #2 uzduotys piestukai FOR CYCLE

--- index.php
+++ index.php
@@ -6,47 +6,6 @@ date_default_timezone_set('Europe/Vilnius');
 
 
 
-$days = 365;
-$cigs_mon_fri = rand(3, 4);
-$cigs_sat = rand(10, 20);
-$cigs_sun = rand(1, 3);
-$pack_price = 3.50;
-$time_per_cig = 5; //5min
-
-
-$sum = 0;
-$price = 0;
-
-$sumasuma = 0;
-$price2 = 0;
-
-for ($week = 1; $week <= 52; $week++){
-    $monday = rand(3, 4);
-    $thuesday = rand(3, 4);
-    $wednesday = rand(3, 4);
-    $thursday = rand(3, 4);
-    $friday = rand(3, 4);
-    $saturday = rand(10, 20);
-    $sunday = rand(1, 3);  
-
-    $week_sum = $monday + $thuesday + $wednesday + $thursday + $friday + $saturday + $sunday;
-
-    $sum += $week_sum;
-
-    $weeksumasuma = $monday + $thuesday + $wednesday + $thursday + $friday;
-
-    $sumasuma += $weeksumasuma;
-}
-
-$price = ceil($sum/10) * $pack_price;
-$price2 = ceil($sumasuma/10) * $pack_price;
-
-$cizai = ceil($sum/10);
-
-$total = round($sum * $time_per_cig / 60);
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -58,25 +17,26 @@ $total = round($sum * $time_per_cig / 60);
     <link rel="stylesheet" href="style.css">
 </head>
 <style>
-.cizas {
-    background-image: url('images/cizas.jpg')
+.piestukas {
+    background-image: url('images/pencil.png')
 }
 </style>
 
 <body>
 
-<h1>Mano dumu sskaiciuokle</h1>
-<h2>Per <?php print $days; ?> dienas, surukysiu <?php print $sum; ?> cigareciu uz <?php print $price; ?> eur.</h2>
 
-<h3>Nerukydamas darbo dienomis sutaupyciau <?php print $price2; ?> eur.</h3>
 
-<h3>Viso traukdamas kiekviena diena prastovesiu valandu <?php print $total;?></h3>
-
-<div style="display: flex; flex-wrap: wrap;">
-    <?php for ($x = 1; $x <= $cizai; $x++): ?>
-        <div style="height: 50px; width: 50px; background-size: cover; margin: 5px; border: 1px solid black; padding: 5px;" class="cizas"></div>
+    <?php for ($x = 1; $x <= 5; $x++): ?>
+        <div style="height: 100px; width: 100px; background-size: cover; margin: 5px; border: 1px solid black; padding: 5px;" class="piestukas"></div>
+        <div><?php print $x; ?></div>
     <?php endfor; ?>
-</div>
 
+    <?php for ($x = 1, $y = 5; $x <= 5; $x++, $y-- ): ?>
+        <div style="height: 100px; width: 100px; background-size: cover; margin: 5px; border: 1px solid black; padding: 5px;" class="piestukas"></div>
+        <div><?php print $y; ?></div>
+    <?php endfor; ?>
+
+
+    
 </body>
 </html>
